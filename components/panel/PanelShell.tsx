a71e338c669614d2fdcc7a7980dbd5204a4a117f
legodgyn <157330002+legodgyn@users.noreply.github.com>
2026-03-09 23:00:25 -0300

    Update PanelShell.tsx

--- components/panel/PanelShell.tsx
+++ components/panel/PanelShell.tsx
@@ -213,8 +213,8 @@ export default function PanelShell({ children }: { children: React.ReactNode }) 
   <Image
     src="/logo.png"
     alt="PL Painel"
-    width={40}
-    height={16}
+    width={200}
+    height={80}
     className="h-8 w-8"
   />
 </div>
