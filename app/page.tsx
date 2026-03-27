1147c0d84413ee8980031d35bdd94f3feab6f360
legodgyn <157330002+legodgyn@users.noreply.github.com>
2026-03-27 18:23:36 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -74,14 +74,16 @@ export default function HomePage() {
             </Link>
           </div>
         </div>
-      </header>
-<!-- Google Tag Manager -->
+
+        <!-- Google Tag Manager -->
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-WQ83H2MH');</script>
 <!-- End Google Tag Manager -->
+      </header>
+
       {/* Hero */}
       <section className="relative overflow-hidden">
         <div className="pointer-events-none absolute inset-0 opacity-60">
