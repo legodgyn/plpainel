a6e58c7af744d1f80dd398c335df8e0f16f8f9cc
legodgyn <157330002+legodgyn@users.noreply.github.com>
2026-04-24 20:25:15 -0300

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -4,16 +4,18 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: [
       {
-        userAgent: "facebookexternalhit",
-        allow:: "/",
-      },
-      {
-        userAgent: "Facebot",
-        allow:: "/",
+        userAgent: [
+          "facebookexternalhit",
+          "Facebot",
+          "FacebookBot",
+          "Meta-ExternalAgent",
+          "Meta-ExternalFetcher",
+        ],
+        allow: "/",
       },
       {
         userAgent: "*",
-        allow:: "/",
+        allow: "/",
       },
     ],
     sitemap: "https://plpainel.com/sitemap.xml",
