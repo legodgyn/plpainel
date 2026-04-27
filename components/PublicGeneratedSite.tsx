a6118b45ee7399f73615a10b6dae18dd25fd1b89
legodgyn <157330002+legodgyn@users.noreply.github.com>
2026-04-27 01:30:34 -0300

    Delete components/PublicGeneratedSite.tsx

--- components/PublicGeneratedSite.tsx
+++
@@ -1,5 +0,0 @@
-import PublicSitePage from "@/app/s/[slug]/page";
-
-export default async function PublicGeneratedSite({ slug }: { slug: string }) {
-  return <PublicSitePage params={{ slug }} />;
-}
