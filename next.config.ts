77556408e0c640eecdc9caf8759db96c8fe7d0fd
Leandro Caetano <leandroeuroenge@gmail.com>
2026-05-08 14:11:24 -0300

    Serve Next assets from main domain

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  assetPrefix:
+    process.env.NODE_ENV === "production" ? "https://plpainel.com" : undefined,
 };
 
 export default nextConfig;
