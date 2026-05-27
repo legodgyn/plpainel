24534134a529554b8b163fdb7cd9e533b4b71171
Leandro Caetano <leandroeuroenge@gmail.com>
2026-05-27 13:40:50 -0300

    Render meta tags on custom domains

--- app/d/[domain]/page.tsx
+++ app/d/[domain]/page.tsx
@@ -1,10 +1,15 @@
-import PublicSitePage from "@/app/s/[slug]/page";
+import PublicSitePage, { generateMetadata as generatePublicSiteMetadata } from "@/app/s/[slug]/page";
 
 type Props = {
   params: { domain: string } | Promise<{ domain: string }>;
 };
 
 export const dynamic = "force-dynamic";
+
+export async function generateMetadata(props: Props) {
+  const { domain } = await Promise.resolve(props.params);
+  return generatePublicSiteMetadata({ params: { slug: domain } });
+}
 
 export default async function DomainPage(props: Props) {
   const { domain } = await Promise.resolve(props.params);
